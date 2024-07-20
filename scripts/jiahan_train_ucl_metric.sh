e42e4d3d1084ccb2fb1a937a28d31b5b638acbd4
jiahan <hanj.micro@gmail.com>
2024-07-20 17:24:27 +0800

    初步开始微调small模型
    Signed-off-by: jiahan <hanj.micro@gmail.com>

---
+++ scripts/jiahan_train_ucl_metric.sh
@@ -0,0 +1,7 @@
+python metric_depth/train.py \
+--encoder vits \
+--dataset UCL \
+--max-depth 200 \
+--epochs 10 \
+--save-path ./save_checkpoints/vits_ucl_metric \
+--pretrained-from checkpoints/depth_anything_v2_metric_hypersim_vits.pth
